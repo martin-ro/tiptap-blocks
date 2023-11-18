0cab59d1622e9935d53c58305d402f91c159e9f8
Martin Ro <martin-ro@users.noreply.github.com>
2023-11-19 04:25:28 +0800

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\TiptapBlocks\BatmanBlock;
+use FilamentTiptapEditor\TiptapEditor;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        TiptapEditor::configureUsing(function (TiptapEditor $component) {
+            $component
+                ->blocks([
+                    BatmanBlock::class,
+                ]);
+        });
     }
 
     /**
